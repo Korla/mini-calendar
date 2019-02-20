03788a96365133342dd0586a89b960869d315637
korla <bengtsson.johannes@gmail.com>
2019-02-20 16:51:51 +0100

    Updates

---
+++ precache-manifest.3a394572bd7b51aadfd3c8dd141a8d69.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "85f6d6497e8ffc5b3e16",
+    "url": "/mini-calendar/static/js/runtime~main.85f6d649.js"
+  },
+  {
+    "revision": "e0ee79da49886427e377",
+    "url": "/mini-calendar/static/js/main.e0ee79da.chunk.js"
+  },
+  {
+    "revision": "67fb1a6effb1cb532895",
+    "url": "/mini-calendar/static/js/2.67fb1a6e.chunk.js"
+  },
+  {
+    "revision": "e0ee79da49886427e377",
+    "url": "/mini-calendar/static/css/main.77fee739.chunk.css"
+  },
+  {
+    "revision": "19f4e290b11466bad2c84a5323175c43",
+    "url": "/mini-calendar/index.html"
+  }
+];
